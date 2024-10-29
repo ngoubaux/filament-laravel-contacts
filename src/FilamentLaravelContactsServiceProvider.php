02b7e136dded6d9a4a684a09ac3690f269119f9a
ngoubaux <ngoubaux@users.noreply.github.com>
2024-10-29 21:53:02 +0000

    Fix styling

--- src/FilamentLaravelContactsServiceProvider.php
+++ src/FilamentLaravelContactsServiceProvider.php
@@ -8,13 +8,13 @@ use Filament\Support\Assets\Css;
 use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
+use goubs\FilamentLaravelContacts\Commands\FilamentLaravelContactsCommand;
+use goubs\FilamentLaravelContacts\Testing\TestsFilamentLaravelContacts;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use goubs\FilamentLaravelContacts\Commands\FilamentLaravelContactsCommand;
-use goubs\FilamentLaravelContacts\Testing\TestsFilamentLaravelContacts;
 
 class FilamentLaravelContactsServiceProvider extends PackageServiceProvider
 {
